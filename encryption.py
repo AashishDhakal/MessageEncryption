a774de8561111238a196160dab41a5e39b0109e2
Aashish Dhakal <33517131+AashishDhakal@users.noreply.github.com>
2018-05-10 12:22:07 +0545

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,19 +1,49 @@
 alphabets="abcdefghijklmnopqrstuvwxyz"
 up_alphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-key=int(input("Enter the secret key to encrypt your messages"))
-Message=input("Enter the Message:")
-newMessage=""
-for character in Message:
-    if character in alphabets:
-        position=alphabets.find(character)
-        newPosition=(position+key)%26
-        newCharacter=alphabets[newPosition]
-        newMessage += newCharacter
-    elif character in up_alphabets:
-        position=up_alphabets.find(character)
-        newPosition=(position+key)%26
-        newCharacter=up_alphabets[newPosition]
-        newMessage += newCharacter
-    else:
-        newMessage += character
-print(newMessage)
+def encrypt():
+    key=int(input("Enter the secret key to encrypt your messages"))
+    Message=input("Enter the Message:")
+    newMessage=""
+    for character in Message:
+        if character in alphabets:
+            position=alphabets.find(character)
+            newPosition=(position+key)%26
+            newCharacter=alphabets[newPosition]
+            newMessage += newCharacter
+        elif character in up_alphabets:
+            position=up_alphabets.find(character)
+            newPosition=(position+key)%26
+            newCharacter=up_alphabets[newPosition]
+            newMessage += newCharacter
+        else:
+            newMessage += character
+    print(newMessage)
+def decrypt():
+    key=int(input("Enter the secret key to decrypt your messages"))
+    Message=input("Enter the Message:")
+    newMessage=""
+    for character in Message:
+        if character in alphabets:
+            position=alphabets.find(character)
+            newPosition=(position-key)%26
+            newCharacter=alphabets[newPosition]
+            newMessage += newCharacter
+        elif character in up_alphabets:
+            position=up_alphabets.find(character)
+            newPosition=(position-key)%26
+            newCharacter=up_alphabets[newPosition]
+            newMessage += newCharacter
+        else:
+            newMessage += character
+    print(newMessage)
+
+print("-----------------------Press E for Encrypt and D for Decrypt---------------")
+user_Choice=input("Do you want to Encrypt or Decrypt?")
+
+if user_Choice=="E":
+    encrypt()
+elif  user_Choice=="D":
+    decrypt()
+else:
+    print("Please enter either E or D")
+
