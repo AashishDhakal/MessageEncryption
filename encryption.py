c89cfcef22f52e61ded1da21ec536450a2b1db5e
Aashish Dhakal <33517131+AashishDhakal@users.noreply.github.com>
2018-05-10 10:10:30 +0545

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,4 +1,5 @@
 alphabets="abcdefghijklmnopqrstuvwxyz"
+up_alphabets="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 key=int(input("Enter the secret key to encrypt your messages"))
 Message=input("Enter the Message:")
 newMessage=""
@@ -7,6 +8,11 @@ for character in Message:
         position=alphabets.find(character)
         newPosition=(position+key)%26
         newCharacter=alphabets[newPosition]
+        newMessage += newCharacter
+    elif character in up_alphabets:
+        position=up_alphabets.find(character)
+        newPosition=(position+key)%26
+        newCharacter=up_alphabets[newPosition]
         newMessage += newCharacter
     else:
         newMessage += character
